9a367b9087dedbce1750831b19fc3b9817eec838
wonder-H <soimar@naver.com>
2023-01-09 22:25:01 +0900

    feat: 404 기본문구 적용

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 export default function NotFound() {
   return (
     <div>
-      NotFound
-      <h2>Nothing to see here!</h2>
+      <h2>404 Not Found 😭</h2>
+      <h3>페이지를 찾을 수 없습니다</h3>
+      <p>요청한 페이지를 찾을 수 없습니다. 사이트 관리자에게 문의해 주세요</p>
       <p>
-        <Link to="/">Go to the home page</Link>
+        <Link to="/">홈으로 이동</Link>
       </p>
     </div>
   );
