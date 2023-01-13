83aa77a7bcdc5b20ce15058514aceac32c46079a
wonder-H <soimar@naver.com>
2023-01-13 16:02:45 +0900

    feat: 마이페이지 구성리스트 나열

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 function MyPage() {
-  return <div>MyPage</div>;
+  return (
+    <div>
+      <div>유저 id </div>
+      <div>유저 닉네임 </div>
+      <div>비밀번호 변경하기 </div>
+      <div>닉네임 변경하기 </div>
+      <div>초대 수락/거절 메세지 보여주는 곳</div>
+      <div>
+        공유한 목록
+        <p>리스트</p>
+      </div>
+      <div>
+        공유받은 목록
+        <p>리스트</p>
+      </div>
+    </div>
+  );
 }
 
 export default MyPage;
