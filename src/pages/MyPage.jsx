fd1dc83b0c0e16be6d443f2a72136df55f35d36c
wonder-H <soimar@naver.com>
2023-01-14 19:53:01 +0900

    design: 마이페이지 기본 css 적용

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,22 +1,164 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledMyPage = styled.div`
+  margin: 0 auto;
+  min-width: 1500px;
+  min-height: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 30px;
+
+  .info {
+    h2 {
+      font-size: 20px;
+      font-weight: 600;
+      margin-bottom: 30px;
+    }
+    p {
+      margin: 15px 0;
+    }
+  }
+
+  .info-btn {
+    button {
+      height: 40px;
+      color: #fff;
+      border-radius: 5px;
+      padding: 10px 25px;
+      background: transparent;
+      cursor: pointer;
+      transition: all 0.3s ease;
+      display: inline-block;
+      box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+        7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+      outline: none;
+      margin: 5px 0 10px;
+      background: linear-gradient(0deg, rgba(120, 120, 120) 1%, rgba(150, 150, 150, 1) 100%);
+      border: none;
+
+      :hover {
+        background: linear-gradient(0deg, rgba(100, 100, 100, 1) 0%, rgba(130, 130, 130, 1) 100%);
+      }
+    }
+  }
+  .invite {
+    min-width: 1420px;
+    height: 180px;
+    margin: 20px;
+    padding: 10px 0 20px;
+    overflow: auto;
+
+    .invite-msg li {
+      margin: 10px 0px 25px;
+      padding: 10px 20px;
+      box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+        7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+      background-color: #ffffffab;
+      button {
+        color: #fff;
+        border-radius: 3px;
+        padding: 8px 15px;
+
+        background: transparent;
+        cursor: pointer;
+        transition: all 0.3s ease;
+        display: inline-block;
+        box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+          7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+        outline: none;
+        margin: 10px 0px 10px;
+        background: linear-gradient(0deg, rgba(120, 120, 120) 1%, rgba(150, 150, 150, 1) 100%);
+        border: none;
+        :hover {
+          background: linear-gradient(0deg, rgba(100, 100, 100, 1) 0%, rgba(130, 130, 130, 1) 100%);
+        }
+      }
+    }
+  }
+
+  .lists {
+    display: flex;
+    margin: 30px 0;
+
+    div {
+      width: 700px;
+      box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+        7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+      outline: none;
+      padding: 10px;
+      background-color: rgba(202, 202, 202, 0.6);
+      margin: 0 50px 20px 0px;
+    }
+    h3 {
+      font-size: 20px;
+      font-weight: 600;
+      margin-bottom: 30px;
+    }
+    li {
+      margin: 20px 0;
+      padding: 10px;
+    }
+  }
+`;
 
 function MyPage() {
   return (
-    <div>
-      <div>유저 id </div>
-      <div>유저 닉네임 </div>
-      <div>비밀번호 변경하기 </div>
-      <div>닉네임 변경하기 </div>
-      <div>초대 수락/거절 메세지 보여주는 곳</div>
-      <div>
-        공유한 목록
-        <p>리스트</p>
+    <StyledMyPage>
+      <div className="info">
+        <h2>어서오세요! 😀</h2>
+        <p>유저 id 공간</p>
+        <p>유저 닉네임 공간</p>
       </div>
-      <div>
-        공유받은 목록
-        <p>리스트</p>
+      <div className="info-btn">
+        <button>비밀번호 변경</button>
+        <button>닉네임 변경</button>
       </div>
-    </div>
+      <div className="invite">
+        <ul className="invite-msg">
+          <li>
+            <h2>??? 님이 유저님과 영화를 공유하고 싶어합니다!</h2>
+            <button>수락</button>
+            <button>거절</button>
+          </li>
+          <li>
+            <h2>??? 님이 유저님과 영화를 공유하고 싶어합니다!</h2>
+            <button>수락</button>
+            <button>거절</button>
+          </li>
+          <li>
+            <h2>??? 님이 유저님과 영화를 공유하고 싶어합니다!</h2>
+            <button>수락</button>
+            <button>거절</button>
+          </li>
+          <li>
+            <h2>??? 님이 유저님과 영화를 공유하고 싶어합니다!</h2>
+            <button>수락</button>
+            <button>거절</button>
+          </li>
+        </ul>
+      </div>
+      <div className="lists">
+        <div className="lists-share">
+          <h3>내가 공유한 장르</h3>
+          <ul>
+            <li>리스트</li>
+            <li>리스트</li>
+            <li>리스트</li>
+          </ul>
+        </div>
+        <div className="lists-shared">
+          <h3>친구에게 공유받은 장르</h3>
+          <ul>
+            <li>리스트</li>
+            <li>리스트</li>
+            <li>리스트</li>
+          </ul>
+        </div>
+      </div>
+    </StyledMyPage>
   );
 }
 
